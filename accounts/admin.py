320c27a474999d5b155841a583a129236d0875c4
Rahathossaincoder <abumdrahat@gmail.com>
2026-05-20 15:45:59 +0600

    on going day-2 part 3

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from tasks.models import (
+    User
+)
+
+# Register each model
+admin.site.register(User)
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from tasks.models import (
+    Semester, Course, ClassMeeting, RoutinePreset,
+    AcademicEvent, Task, Habit, HabitCheckin
+)
+
+# Register each model
+admin.site.register(Semester)
+admin.site.register(Course)
+admin.site.register(ClassMeeting)
+admin.site.register(RoutinePreset)
+admin.site.register(AcademicEvent)
+admin.site.register(Task)
+admin.site.register(Habit)
+admin.site.register(HabitCheckin)
